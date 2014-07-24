bc97f851842b313acf04e099fc95f94676a05894
Travis Jeffery <tj@travisjeffery.com>
2014-07-24 17:37:38 -0500

    Fix import of foundation - no need for uikit

--- TRVSDictionaryWithCaseInsensitivity/TRVSDictionaryWithCaseInsensitivity.h
+++ TRVSDictionaryWithCaseInsensitivity/TRVSDictionaryWithCaseInsensitivity.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Travis Jeffery. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for TRVSDictionaryWithCaseInsensitivity.
 FOUNDATION_EXPORT double TRVSDictionaryWithCaseInsensitivityVersionNumber;
