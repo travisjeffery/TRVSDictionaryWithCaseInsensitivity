98ae7d390923ef91c0a19de81b03f4157e58fc51
Travis Jeffery <tj@travisjeffery.com>
2014-07-24 17:38:58 -0500

    Fix import of foundation

--- TRVSDictionaryWithCaseInsensitivityTests/TRVSDictionaryWithCaseInsensitivityTests.m
+++ TRVSDictionaryWithCaseInsensitivityTests/TRVSDictionaryWithCaseInsensitivityTests.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Travis Jeffery. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 #import <XCTest/XCTest.h>
 #import "TRVSDictionaryWithCaseInsensitivity.h"
 
